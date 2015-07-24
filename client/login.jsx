4e95c644aeb6aadfe7b38fe030d823265205453d
toanpp <toanpp@twin.vn>
2015-07-24 16:22:09 +0700

    Login with facebook, google

--- client/login.jsx
+++ client/login.jsx
@@ -11,6 +11,7 @@ const {
 
 const ThemeManager = new mui.Styles.ThemeManager();
 
+
 Login = React.createClass({
   // This mixin makes the getMeteorData method work
   childContextTypes: {
@@ -22,27 +23,63 @@ Login = React.createClass({
       muiTheme: ThemeManager.getCurrentTheme()
     };
   },
+  isFirstLogin() {
+    var userId = Meteor.userId();
+    var lstUserProfile = UserInfo.find({
+      UserId: userId
+    }).fetch();
+    if (lstUserProfile && lstUserProfile.length > 0) {
+
+      return false;
+    }
+    return true;
+  },
   toggleToSignUp() {
     React.render(<Signup />, document.getElementById("render-target"));
   },
-
+  toggleFacebookLogin(){
+    Meteor.loginWithFacebook(function(err, res) {
+      if (err) {
+        $('div[id="errorMessage"]').text("Can't connect to Facebook.");
+      } else {
+        console.log(this.isFirstLogin());
+        if (this.isFirstLogin()) {
+          console.log("THIS IS FIRST LOGIN");
+        }
+      }
+    });
+  },
+  toggleGoogleLogin(){
+    Meteor.loginWithGoogle(function(err, res) {
+      if (err) {
+        $('div[id="errorMessage"]').text("Can't connect to Google.");
+      } else {
+        console.log(this.isFirstLogin());
+        if (this.isFirstLogin()) {
+          console.log("THIS IS FIRST LOGIN");
+        }
+      }
+    });
+  },
   render() {
     return (
       <div className="container">
       <AppBar
-      title="Login"/>
+      title="LOGIN TO SOCIAL LINGU"/>
       <div className="email-field">
       <TextField
-      hintText="Your email" id="email-login"/>
+      hintText="Your email" id="email-login" fullWidth={true}/>
       </div>
       <div className="password-field">
       <TextField
-      hintText="Your password" />
+      hintText="Your password" fullWidth={true}/>
       </div>
       <div className="function-button" >
       <a href="#">Forgot your password?</a>
       <RaisedButton label="Login" secondary={true} fullWidth={true}/>
       <RaisedButton label="Sign up" secondary={true} fullWidth={true} onClick={this.toggleToSignUp} />
+      <RaisedButton label="Login with Facebook" secondary={true} fullWidth={true} onClick={this.toggleFacebookLogin}/>
+      <RaisedButton label="Login with Google" secondary={true} fullWidth={true} onClick={this.toggleGoogleLogin}/>
       </div>
       </div>
     );
